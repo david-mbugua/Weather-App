d5f346328556dc2ac55fe114a704f273f1c46bce
david-mbugua <47davidmbugua23@gmail.com>
2023-04-13 18:11:27 +0300

    Added all the needed components

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -4,11 +4,11 @@ import {
   StyleSheet,
   Text,
   FlatList,
-  View,
   StatusBar,
   ImageBackground
 } from 'react-native'
 import { Feather } from '@expo/vector-icons'
+import ListItem from '../components/ListIrem'
 
 const DATA = [
   {
@@ -62,18 +62,19 @@ const Item = (props) => {
 }
 const UpcomingWeather = () => {
   const renderItem = ({ item }) => (
-    <Item
+    <ListItem
       condition={item.weather[0].main}
       dt_txt={item.dt_txt}
       min={item.main.temp_min}
       max={item.main.temp_max}
     />
   )
+  const {container, image } = styles
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={container}>
       <ImageBackground
         source={require('../../assets/upcoming-background.jpg')}
-        style={styles.image}
+        style={image}
       >
         <Text>Upcoming Wather</Text>
         <FlatList
@@ -91,25 +92,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
     backgroundColor: 'royalblue'
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    borderWidth: 3,
-    borderRadius: 20,
-    backgroundColor: 'deepskyblue'
-  },
-  temp: {
-    color: 'white',
-    fontSize: 20
-  },
-  date: {
-    color: 'white',
-    fontSize: 15
   },
   image: {
     flex: 1
