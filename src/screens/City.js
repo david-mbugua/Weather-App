438963c3c196b3c747bc26bbe612541a20c30381
david-mbugua <47davidmbugua23@gmail.com>
2023-04-14 20:18:23 +0300

    Final touches

--- src/screens/City.js
+++ src/screens/City.js
@@ -7,10 +7,10 @@ import {
   StatusBar,
   View
 } from 'react-native'
-import { Feather } from '@expo/vector-icons'
+import moment from 'moment'
 import IconText from '../components/IconText'
 
-const City = () => {
+const City = ({ weatherData }) => {
   const {
     container,
     cityName,
@@ -23,19 +23,20 @@ const City = () => {
     rowLayout,
     imageLayout
   } = styles
+  const { name, country, population, sunrise, sunset } = weatherData
   return (
     <SafeAreaView style={container}>
       <ImageBackground
         source={require('../../assets/city-background.jpg')}
         style={imageLayout}
       >
-        <Text style={[cityName, cityText]}>Nairobi</Text>
-        <Text style={[countryName, cityText]}>Kenya</Text>
+        <Text style={[cityName, cityText]}>{name}</Text>
+        <Text style={[countryName, cityText]}>{country}</Text>
         <View style={[populationWrapper, rowLayout]}>
           <IconText
             iconName={'user'}
             iconColor={'red'}
-            bodyText={'47000'}
+            bodyText={`population: ${population}`}
             bodyTextStyles={populationText}
           />
         </View>
@@ -43,13 +44,13 @@ const City = () => {
           <IconText
             iconName={'sunrise'}
             iconColor={'white'}
-            bodyText={'10:46:58am'}
+            bodyText={moment(sunrise).format('h:mm:ss a')}
             bodyTextStyles={riseSetText}
           />
           <IconText
             iconName={'sunset'}
             iconColor={'white'}
-            bodyText={'17:28:15pm'}
+            bodyText={moment(sunset).format('h:mm:ss a')}
             bodyTextStyles={riseSetText}
           />
         </View>
--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -2,52 +2,12 @@ import React from 'react'
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
   FlatList,
   StatusBar,
   ImageBackground
 } from 'react-native'
 import { Feather } from '@expo/vector-icons'
-import ListItem from '../components/ListIrem'
-
-const DATA = [
-  {
-    dt_txt: '2023-02-18 12:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55
-    },
-    weather: [
-      {
-        main: 'clear'
-      }
-    ]
-  },
-  {
-    dt_txt: '2023-02-18 15:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55
-    },
-    weather: [
-      {
-        main: 'clouds'
-      }
-    ]
-  },
-  {
-    dt_txt: '2023-02-18 18:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55
-    },
-    weather: [
-      {
-        main: 'Rain'
-      }
-    ]
-  }
-]
+import ListItem from '../components/ListItem'
 
 const Item = (props) => {
   const { dt_txt, min, max, condition } = props
@@ -60,7 +20,7 @@ const Item = (props) => {
     </View>
   )
 }
-const UpcomingWeather = () => {
+const UpcomingWeather = ({ weatherData }) => {
   const renderItem = ({ item }) => (
     <ListItem
       condition={item.weather[0].main}
@@ -76,9 +36,8 @@ const UpcomingWeather = () => {
         source={require('../../assets/upcoming-background.jpg')}
         style={image}
       >
-        <Text>Upcoming Wather</Text>
         <FlatList
-          data={DATA}
+          data={weatherData}
           renderItem={renderItem}
           keyExtractor={(item) => item.dt_txt}
         />
@@ -91,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: 'royalblue'
+    backgroundColor: 'cadetblue'
   },
   image: {
     flex: 1
